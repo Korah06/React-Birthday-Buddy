e28fbff5e5a94e3b074d3be8612e0668b4de977c
Mario Espasa <mario.espasa.2003@gmail.com>
2023-05-26 12:15:13 +0200

    made it

---
+++ src/Person.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Person({ image, name, age }) {
+  return (
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
+}
